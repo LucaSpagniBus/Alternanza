b955b74445ae55135ed8b1da1a76313f8cb977d7
luca <lucaspagni.bus@gmail.com>
2018-10-18 10:07:24 +0200

    esercizi

---
+++ Counter/src/App.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+
+  state = {
+      counter: 0
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Counter: {this.state.counter}</h1>
+        <button onClick={() => this.setState({counter: this.state.counter+1})}>
+            Increment
+        </button>
+        <button onClick={() => this.setState({counter: this.state.counter-1})}>
+            Decrement
+        </button>
+        <button onClick={() => this.setState({counter: this.state.counter*2})}>
+            Double
+        </button>
+        <button onClick={() => this.setState({counter: this.state.counter/2})}>
+            Half
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ toDo/src/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+
+  state = {
+      currentToDo:'',
+      toDo: []
+  }
+
+  onSubmitText(event){
+    event.preventDefault();
+    this.setState({ toDo: [...this.state.toDo, this.state.currentToDo]});
+  }
+
+  onChangeText(event){
+    this.setState({currentToDo: event.target.value});
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>ToDo List</h1>
+        <form onSubmit={(event) => this.onSubmitText(event)}>
+          <input onChange={(event) => this.onChangeText(event)} type="text" id="dato"></input>
+          <button>Submit</button>
+        </form>
+        <ol>
+          {
+            this.state.toDo.map((toDo,index) => <li key={index}>{toDo}</li>)
+          }
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default App;
